cfdfc38b42837f640f86aa3acde0cdd07bc8ff29
Mauricio Matsumura <mhmatsumura@yahoo.com.br>
2018-08-13 00:23:28 -0300

    retirando os imports das page deletadas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
-import { TabsPage } from '../pages/tabs/tabs';
+
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -16,10 +13,7 @@ import { RomaneioPageModule } from '../pages/romaneio/romaneio.module';
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
+    
   ],
   imports: [
     BrowserModule,
@@ -30,10 +24,7 @@ import { RomaneioPageModule } from '../pages/romaneio/romaneio.module';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
+    
   ],
   providers: [
     StatusBar,
